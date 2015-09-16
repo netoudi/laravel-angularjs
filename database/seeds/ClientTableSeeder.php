19dd6b425ab4308c308a76ef668d5129c2492b54
netoudi <netoudi@rocketmail.com>
2015-09-15 23:12:08 -0300

    Criando Model Client.

---
+++ database/seeds/ClientTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use CodeProject\Client;
+use Illuminate\Database\Seeder;
+
+class ClientTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Client::class, 10)->create();
+    }
+}
