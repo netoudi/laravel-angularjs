88905b25621802ce5ecc1a98ebdc3dc2235bdd56
netoudi <netoudi@rocketmail.com>
2015-09-15 23:47:31 -0300

    Criando Controller Client.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/client', 'ClientController@index');
+Route::post('/client', 'ClientController@store');
+Route::get('/client/{id}', 'ClientController@show');
+Route::put('/client/{id}', 'ClientController@update');
+Route::delete('/client/{id}', 'ClientController@destroy');
