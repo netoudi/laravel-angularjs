794cdf2a4df8db48b2ec0d91b3253b52b74b8f0e
netoudi <netoudi@rocketmail.com>
2015-10-04 17:00:19 -0300

    Implementação da tela de login.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="app">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -63,9 +63,9 @@
     </div>
 </nav>
 
-@yield('content')
+<div ng-view></div>
 
-        <!-- Scripts -->
+<!-- Scripts -->
 @if(Config::get('app.debug'))
     <script src="{{ asset('build/js/vendor/jquery.min.js') }}"></script>
     <script src="{{ asset('build/js/vendor/angular.min.js') }}"></script>
@@ -75,6 +75,13 @@
     <script src="{{ asset('build/js/vendor/angular-messages.min.js') }}"></script>
     <script src="{{ asset('build/js/vendor/ui-bootstrap.min.js') }}"></script>
     <script src="{{ asset('build/js/vendor/navbar.min.js') }}"></script>
+    <script src="{{ asset('build/js/vendor/angular-cookies.min.js') }}"></script>
+    <script src="{{ asset('build/js/vendor/query-string.js') }}"></script>
+    <script src="{{ asset('build/js/vendor/angular-oauth2.min.js') }}"></script>
+
+    <script src="{{ asset('build/js/app.js') }}"></script>
+    <script src="{{ asset('build/js/controllers/home.js') }}"></script>
+    <script src="{{ asset('build/js/controllers/login.js') }}"></script>
 @else
     <script src="{{ elixir('js/all.js') }}"></script>
 @endif
