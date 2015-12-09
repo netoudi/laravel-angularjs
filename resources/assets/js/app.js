ec0a97e46299be24d8e22194e6b74fd7e3b6d200
netoudi <netoudi@rocketmail.com>
2015-12-09 10:12:19 -0200

    Organizando baseUrl com provider.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,7 +2,22 @@ var app = angular.module('app', ['ngRoute', 'angular-oauth2', 'app.controllers']
 
 angular.module('app.controllers', ['angular-oauth2']);
 
-app.config(['$routeProvider', 'OAuthProvider', function ($routeProvider, OAuthProvider) {
+app.provider('appConfig', function() {
+    var config = {
+        baseUrl: 'http://127.0.0.1:8080'
+    };
+
+    return {
+        config: config,
+        $get: function() {
+            return config;
+        }
+    }
+});
+
+app.config([
+    '$routeProvider', 'OAuthProvider', 'appConfigProvider',
+    function ($routeProvider, OAuthProvider, appConfigProvider) {
     $routeProvider
         .when('/login', {
             templateUrl: 'build/views/login.html',
@@ -14,7 +29,7 @@ app.config(['$routeProvider', 'OAuthProvider', function ($routeProvider, OAuthPr
         });
 
     OAuthProvider.configure({
-        baseUrl: 'http://localhost:8000',
+        baseUrl: appConfigProvider.config.baseUrl,
         clientId: 'appid1',
         clientSecret: 'secret',
         grantPath: 'oauth/access_token'
