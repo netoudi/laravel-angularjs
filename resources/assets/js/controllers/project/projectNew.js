e4bf24a7a484aff3662474f77d1e6cb71f0a8923
netoudi <netoudi@rocketmail.com>
2015-12-21 22:55:59 -0200

    Refatoração do typeahead no formulário de Projetos.

--- resources/assets/js/controllers/project/projectNew.js
+++ resources/assets/js/controllers/project/projectNew.js
@@ -2,25 +2,21 @@ angular.module('app.controllers')
     .controller('ProjectNewController',
         ['$scope', '$location', '$cookies', 'Project', 'Client', 'appConfig',
             function ($scope, $location, $cookies, Project, Client, appConfig) {
-                $scope.project = new Project({owner_id: $cookies.getObject('user').id});
-                $scope.clients = Client.query();
+                $scope.project = new Project();
                 $scope.status = appConfig.project.status;
 
                 $scope.save = function () {
                     if ($scope.form.$valid) {
+                        $scope.project.owner_id = $cookies.getObject('user').id;
                         $scope.project.$save().then(function () {
                             $location.path('/project');
                         });
                     }
                 };
 
-                $scope.formatName = function (id) {
-                    if (id) {
-                        for (var i in $scope.clients) {
-                            if ($scope.clients[i].id == id) {
-                                return $scope.clients[i].name;
-                            }
-                        }
+                $scope.formatName = function (model) {
+                    if (model) {
+                        return model.name;
                     }
                     return '';
                 };
@@ -31,6 +27,10 @@ angular.module('app.controllers')
                             searchFields: 'name:like'
                         }
                     ).$promise;
+                };
+
+                $scope.selectClient = function (item) {
+                    $scope.project.client_id = item.id;
                 };
 
             }]);
