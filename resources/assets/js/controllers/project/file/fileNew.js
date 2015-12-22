fe502d89dd4a12b754ef07471c9e0e0ae7860c07
netoudi <netoudi@rocketmail.com>
2015-12-22 12:05:16 -0200

    Atualização no cadastro de arquivos do módulo Projetos.

--- resources/assets/js/controllers/project/file/fileNew.js
+++ resources/assets/js/controllers/project/file/fileNew.js
@@ -1,15 +1,23 @@
 angular.module('app.controllers')
     .controller('ProjectFileNewController',
-        ['$scope', '$location', '$routeParams', 'Project', 'ProjectFile',
-            function ($scope, $location, $routeParams, Project, ProjectFile) {
+        ['$scope', '$location', '$routeParams', 'Project', 'appConfig', 'Url', 'Upload',
+            function ($scope, $location, $routeParams, Project, appConfig, Url, Upload) {
                 $scope.project = Project.get({id: $routeParams.id});
-                $scope.file = new ProjectFile({id: $routeParams.id});
 
                 $scope.save = function () {
                     if ($scope.form.$valid) {
-                        $scope.file.$save().then(function () {
+                        var url = appConfig.baseUrl + Url.getUrlFromUrlSymbol(appConfig.urls.projectFile, {id: $routeParams.id, idFile: ''});
+                        Upload.upload({
+                            url: url,
+                            fields: {
+                                name: $scope.file.name,
+                                description: $scope.file.description
+                            },
+                            file: $scope.file.file
+                        }).success(function (data, status, headers, config) {
                             $location.path('/project/' + $routeParams.id + '/file');
                         });
                     }
-                }
+                };
+
             }]);
